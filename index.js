e829b6923c5d0aba59a754b5b8f5a01530cfa31a
biplob4 <biplobhossen6020@gmail.com>
2022-05-16 20:18:30 +0600

    adding all

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -13,10 +14,130 @@ app.use(express.json());
 // connection mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ubki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyjwt(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbedden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("doctors-portal").collection("service");
+        const bookingCollection = client.db("doctors-portal").collection("booking");
+        const usersCollection = client.db("doctors-portal").collection("users");
+
+        app.get('/users', verifyjwt, async (req, res) => {
+            const users = await usersCollection.find().toArray();
+            res.send(users);
+        })
+
+        app.get('/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user = await usersCollection.findOne({email: email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin})
+          })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const user = req.body;
+            const option = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, option);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ result, token });
+        })
+
+        app.put('/user/admin/:email', verifyjwt, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await usersCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
+        })
+
+        /**
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking 
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id) //
+     * app.delete('/booking/:id) //
+    */
+
+        app.use('/booking', verifyjwt, async (req, res) => {
+            const patient = req.query.patient;
+            const decoddedEmail = req.decoded.email;
+            if (patient === decoddedEmail) {
+                const query = { patient: patient };
+                const bokking = await bookingCollection.find(query).toArray();
+                return res.send(bokking);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' });
+            }
+        })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            const exixts = await bookingCollection.findOne(query);
+            if (exixts) {
+                return res.send({ success: false, bokking: exixts })
+            }
+            const rasult = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, rasult });
+        })
+
+
+        // Warning: This is not the proper way to query multiple collection. 
+        // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+            // step 1:  get all services
+            const services = await serviceCollection.find().toArray();
+
+            // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // step 3: for each service
+            services.forEach(service => {
+                // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                // step 5: select slots for the service Bookings: ['', '', '', '']
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                // step 6: select those slots that are not in bookedSlots
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                //step 7: set available to slots to make it easier 
+                service.slots = available;
+            });
+            res.send(services);
+        })
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -24,6 +145,7 @@ async function run() {
             const service = await cursor.toArray();
             res.send(service);
         })
+
     }
     finally { }
 }
